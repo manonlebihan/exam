2510a036f5a807ca5b216dbf310e4954f42b817b
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-06-08 11:51:25 +0200

    commit 23

---
+++ level_1/ft_atoi.c
@@ -0,0 +1,22 @@
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	res = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (sign * res);
+}
