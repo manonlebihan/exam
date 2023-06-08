fd1a5a697d475a65161a1d5de6b79c2eeee141a9
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-06-08 11:02:13 +0200

    commit 20

--- level_1/camel_to_snake.c
+++ level_1/camel_to_snake.c
@@ -1,14 +1,32 @@
-/* Assignment name  : camel_to_snake
-Expected files   : camel_to_snake.c
-Allowed functions: malloc, realloc, write
---------------------------------------------------------------------------------
+#include <unistd.h>
 
-Write a program that takes a single string in lowerCamelCase format
-and converts it into a string in snake_case format.
+void	camel_to_snake(char *str)
+{
+	int	i;
 
-A lowerCamelCase string is a string where each word begins with a capital letter
-except for the first one.
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+			write(1, "_", 1);
+			write(1, &str[i], 1);
+		}
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
 
-A snake_case string is a string where each word is in lower case, separated by
-an underscore "_". */
-
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (1);
+	}
+	camel_to_snake(argv[1]);
+	return (0);
+}
