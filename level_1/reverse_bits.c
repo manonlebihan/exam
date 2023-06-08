9592259eed2497efda554d6a9b0e5030fac56592
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-06-08 14:12:20 +0200

    commit 27

---
+++ level_1/reverse_bits.c
@@ -0,0 +1,29 @@
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int		i = 8;
+	unsigned char	res = 0;
+
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
+}
+
+unsigned char	reverse_bits(unsigned char octet)
+{
+	int				i;
+	unsigned char	res;
+
+	i = 8;
+	res = 0;
+	while (i > 0)
+	{
+		res = res * 2 + (octet % 2);
+		octet = octet / 2;
+		i--;
+	}
+	return (res);
+}
