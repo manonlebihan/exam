054d4be8301a5c2de29fd75dd8fee75810414e91
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-06-08 10:53:22 +0200

    commit 20

--- level_1/alpha_mirror.c
+++ level_1/alpha_mirror.c
@@ -8,9 +8,13 @@ void	alpha_mirror(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			
+			str[i] = 'z' - (str[i] - 'a');
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] = 'Z' - (str[i] - 'A');
+		write(1, &str[i], 1);
 		i++;
 	}
+	write(1, "\n", 1);
 }
 
 int	main(int argc, char **argv)
