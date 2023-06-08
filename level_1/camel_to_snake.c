a25b5be17fc2efc7bd7646583872a9a5922b7339
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-06-08 10:46:09 +0200

    commit 18

---
+++ level_1/camel_to_snake.c
@@ -0,0 +1,14 @@
+/* Assignment name  : camel_to_snake
+Expected files   : camel_to_snake.c
+Allowed functions: malloc, realloc, write
+--------------------------------------------------------------------------------
+
+Write a program that takes a single string in lowerCamelCase format
+and converts it into a string in snake_case format.
+
+A lowerCamelCase string is a string where each word begins with a capital letter
+except for the first one.
+
+A snake_case string is a string where each word is in lower case, separated by
+an underscore "_". */
+
